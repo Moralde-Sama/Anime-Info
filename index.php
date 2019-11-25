e5743333876505d775515cf5e64d0c04e220755b
Moralde-Sama <moralde.sama@gmail.com>
2019-11-25 22:18:45 +0800

    init
    *added partial landing page design

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Anime-Chan</title>
+    <link rel="stylesheet" href="build/landingpage.css">
+    <link rel="stylesheet" href="node_modules/@fortawesome/fontawesome-free/css/fontawesome.css">
+    <link rel="stylesheet" href="node_modules/@fortawesome/fontawesome-free/css/solid.css">
+</head>
+
+<body>
+    <nav class="navigation-bar" style="background-image: url('images/saoalization-banner.jpg');">
+    <div class="navigation-overlay"></div>
+    </nav>
+    <div class="container" style="margin-top: -10rem;">
+        <div class="columns is-desktop">
+            <div class="column is-4"></div>
+            <div class="column is-4">
+                <div class="card shadow-4">
+                    <img class="card-anime--image" src="images/saoalization.jpg" alt="Sao">
+                    <div class="card-anime--title">
+                        <p>Sword Art Online Alicization</p>
+                    </div>
+                    <div class="card-anime--content">
+                        <a class="options" href="#"><i class="fas fa-ellipsis-v"></i></a>
+                        <p class="episodes">Episode 7 of 25 Episodes airing in</p>
+                        <p class="time-left">5 Days and 1 Hour</p>
+                        <p class="score"><i class="fas fa-smile"><span>75% Score</span></i></p>
+                        <p class="rating"><i class="fas fa-heart"><span>1 Rank</span></i></p>
+                        <p class="description">Second part of Sword Art Online: Alicization.</p>
+                        <ul class="genres">
+                            <li>action</li>
+                            <li>adventure</li>
+                            <li>fantasy</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="column is-4"></div>
+        </div>
+        <div class="columns is-desktop">
+            <div class="column is-4">
+                <div class="card shadow-4">
+                    <img class="card-anime--image" src="images/saoalization.jpg" alt="Sao">
+                    <div class="card-anime--title">
+                        <p>Sword Art Online Alicization</p>
+                    </div>
+                    <div class="card-anime--content">
+                        <a class="options" href="#"><i class="fas fa-ellipsis-v"></i></a>
+                        <p class="episodes">Episode 7 of 25 Episodes airing in</p>
+                        <p class="time-left">5 Days and 1 Hour</p>
+                        <p class="score"><i class="fas fa-smile"><span>75% Score</span></i></p>
+                        <p class="rating"><i class="fas fa-heart"><span>2 Rank</span></i></p>
+                        <p class="description">Second part of Sword Art Online: Alicization.</p>
+                        <ul class="genres">
+                            <li>action</li>
+                            <li>adventure</li>
+                            <li>fantasy</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="column is-4">
+                <div class="card shadow-4">
+                    <img class="card-anime--image" src="images/saoalization.jpg" alt="Sao">
+                    <div class="card-anime--title">
+                        <p>Sword Art Online Alicization</p>
+                    </div>
+                    <div class="card-anime--content">
+                        <a class="options" href="#"><i class="fas fa-ellipsis-v"></i></a>
+                        <p class="episodes">Episode 7 of 25 Episodes airing in</p>
+                        <p class="time-left">5 Days and 1 Hour</p>
+                        <p class="score"><i class="fas fa-smile"><span>75% Score</span></i></p>
+                        <p class="rating"><i class="fas fa-heart"><span>2 Rank</span></i></p>
+                        <p class="description">Second part of Sword Art Online: Alicization.</p>
+                        <ul class="genres">
+                            <li>action</li>
+                            <li>adventure</li>
+                            <li>fantasy</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="column is-4">
+                <div class="card shadow-4">
+                    <img class="card-anime--image" src="images/saoalization.jpg" alt="Sao">
+                    <div class="card-anime--title">
+                        <p>Sword Art Online Alicization</p>
+                    </div>
+                    <div class="card-anime--content">
+                        <a class="options" href="#"><i class="fas fa-ellipsis-v"></i></a>
+                        <p class="episodes">Episode 7 of 25 Episodes airing in</p>
+                        <p class="time-left">5 Days and 1 Hour</p>
+                        <p class="score"><i class="fas fa-smile"><span>75% Score</span></i></p>
+                        <p class="rating"><i class="fas fa-heart"><span>2 Rank</span></i></p>
+                        <p class="description">Second part of Sword Art Online: Alicization.</p>
+                        <ul class="genres">
+                            <li>action</li>
+                            <li>adventure</li>
+                            <li>fantasy</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
